6d148f6960258f94fcba2c03149252e9a4880c53
badalraj444 <badalraj444@gmail.com>
2025-07-25 09:48:08 +0530

    added signout functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,17 @@
 import Image from 'next/image';
 import Search from './Search';
 import FileUploader from './FileUploader';
+import { signOutUser } from '@/lib/actions/user.actions';
 const Header = () => {
   return (
     <header className="hidden items-center justify-between gap-5 p-5 sm:flex lg:py-7 xl:gap-10">
       <Search />
       <div className="flex-center min-w-fit gap-4">
         <FileUploader />
-        <form>
+        <form action={async () => {
+          "use server";
+          await signOutUser();
+        }}>
           <button type="submit" className="sign-out">
             <Image
               src="/assets/icons/logout.svg"
